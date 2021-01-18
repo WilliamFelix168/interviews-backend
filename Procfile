90fc26002409dcbd58a4ad706c41e1788267ab79
william felix <william.felix@pmberjaya.com>
2021-01-18 14:56:27 +0700

    make it better

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: node netlify-lambda serve server
+//tesss
--- server/api.js
+++ server/api.js
@@ -1,17 +1,20 @@
 const express = require("express");
-const serverless = require('serverless-http');
+const serverless = require("serverless-http");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const SERVER_FUNCTIONS = '/.netlify/functions/api';
+const SERVER_FUNCTIONS = "/.netlify/functions/api";
 
 dotenv.config();
 
-
 //Connect to DB
-mongoose.connect(process.env.DB_CONNECT, { useUnifiedTopology: true, useNewUrlParser: true }, () => {
-  console.log("connected to db");
-})
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  () => {
+    console.log("connected to db");
+  }
+);
 
 //Middleware
 app.use(express.json());
